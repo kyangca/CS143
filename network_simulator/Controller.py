4218a39b12f27447c40db513898549eb0d913ff5
Archan Luhar <archanl@gmail.com>
2014-12-09 22:22:53 -0800

    add option to specify interval length and also add legend

--- network_simulator/Controller.py
+++ network_simulator/Controller.py
@@ -5,10 +5,17 @@ from Device import Host, Router
 from EventQueue import EventQueue
 from Flow import Flow
 from Link import Link
+import matplotlib
 from matplotlib import pyplot
 from optparse import OptionParser
 import json
 import numpy
+
+font = {'family' : 'normal',
+        'weight' : 'bold',
+        'size'   : 9}
+
+matplotlib.rc('font', **font)
 
 class Controller(object):
     """The main controller class.
@@ -42,7 +49,10 @@ class Controller(object):
         self._debug = options['debug']
         self._current_time = 0.0
         self._log_interval_start = 0.0
-        self._log_interval_length = 1.0
+        if 'log_interval_length' in options:
+            self._log_interval_length = float(options['log_interval_length'])
+        else:
+            self._log_interval_length = 1.0
         self._event_queue = EventQueue()
 
         self._links = {}
@@ -179,7 +189,6 @@ class Controller(object):
         line = self._logs[log_type]['devices'][device_name]['line']
         line.set_xdata(numpy.append(line.get_xdata(), x))
         line.set_ydata(numpy.append(line.get_ydata(), y))
-        line.set_label(device_name)
         self._logs[log_type]['subplot'].relim()
         self._logs[log_type]['subplot'].autoscale_view()
         pyplot.draw() 
@@ -224,6 +233,7 @@ class Controller(object):
                 'temp_interval_values': [],
             }
             self._logs[log_type]['devices'][device_name]['line'], = self._logs[log_type]['subplot'].plot([], [], label=device_name)
+            self._logs[log_type]['subplot'].legend(bbox_to_anchor=(1.1, 1.0))
 
         if (self._current_time - self._log_interval_length >=
                 self._log_interval_start):
@@ -274,6 +284,8 @@ if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option("-f", "--file", dest="filename",
         help="Test json filename (e.g. test0.json)")
+    parser.add_option("-i", "--interval", dest="log_interval_length",
+        help="Log interval length as float > 0.0")
     parser.add_option("-q", "--quiet", action="store_false", dest="verbose",
         default=True, help="don't print status messages to stdout")
     parser.add_option("--debug", action="store_true")
